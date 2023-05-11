208347d430f3153980f6c1fc18730b4c08e2daec
Kapilkumar042 <87068559+Kapilkumar042@users.noreply.github.com>
2023-05-11 15:39:53 +0530

    upload frontend on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Brand from "./components/brand/Brand";
+import Slider1 from "./components/slider/Slider1";
+import { marvel, slides, slides1, laptop } from "./json/Marval_data";
+import { phone } from "./components/bestselling/data";
+import BestSelling from "./components/bestselling/BestSelling";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Header />
+      <Brand />
+      <BestSelling data={phone} heading="Best Selling Mobile Skins" />
+      <Slider1 slides={slides} interval={2000} />
+      <BestSelling data={marvel} heading="Official Marvel Inspired Designs" />
+      <Slider1 slides={slides1} interval={2000} />
+      <BestSelling data={laptop} heading="Best Selling Laptop Skins" />
+      <Footer />
+    </React.Fragment>
   );
 }
 
